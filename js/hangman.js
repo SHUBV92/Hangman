e246809859e91861a9a85ffc3e8c76ac4f8689a4
your Shubinder Virk <shubinder92@gmail.co.uk>
2020-03-16 15:52:14 +0000

    Game Finished

--- js/hangman.js
+++ js/hangman.js
@@ -28,33 +28,91 @@ function randomWord() {
 }
 
 function generateButtons() {
-    let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter => 
+  let buttonsHTML = "abcdefghijklmnopqrstuvwxyz"
+    .split("")
+    .map(
+      letter =>
         `
         <button 
             class="btn btn-lg btn-primary m-2"
-            id='` + letter + `'
-            onClick="handleGuess('` + letter + `')"
+            id='` +
+        letter +
+        `'
+            onClick="handleGuess('` +
+        letter +
+        `')"
             >
-                ` + letter + `
+                ` +
+        letter +
+        `
                 </button>
-        `).join(``);
+        `
+    )
+    .join(``);
 
-        document.getElementById('keyboard').innerHTML = buttonsHTML;
+  document.getElementById("keyboard").innerHTML = buttonsHTML;
+}
 
+function handleGuess(chosenLetter) {
+  guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+  document.getElementById(chosenLetter).setAttribute("disabled", true);
+
+  alert(answer);
+
+  if (answer.indexOf(chosenLetter) >= 0) {
+    guessedWord();
+  } else if (answer.indexOf(chosenLetter) === -1){
+    mistakes++;
+    updateMistakes();
+    checkIfGameLost();
+    updateHangmanPicture();
+
+  }
+}
+
+function updateHangmanPicture() {
+    document.getElementById('hangmanPic').src = './images/' + mistakes + '.jpg';
+}
+
+function checkIfGameWon (){
+    if(wordStatus === answer){
+        documents.getElementById('keyboard').innerHTML = 'You Win';
     }
+}
 
-
-    function guessedWord(){
-        wordStatus = answer.split('')
-        .map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join("");
-
-        document.getElementById('wordSpotLight').innerHTML = wordStatus;
+function checkIfGameLost (){
+    if(mistakes === maxWrong){
+        document.getElementById('wordSpotLight').innerHTML = "The answer was: " + answer;
+        documents.getElementById('keyboard').innerHTML = 'You Win';
     }
+}
+
+function guessedWord() {
+  wordStatus = answer
+    .split("")
+    .map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ "))
+    .join("");
+
+  document.getElementById("wordSpotLight").innerHTML = wordStatus;
+}
+
+function updateMistakes(){
+    document.getElementById('mistakes').innerHTML = mistakes;
+}
+
+function reset() {
+    mistakes = 0;
+    guessed = [];
+    document.getElementById('hangmanPic').src = './images/1.jpg'
+
+    randomWord();
+    guessedWord();
+    updateMistakes();
+    generateButtons();
+}
 
 
-
-    document.getElementById('maxWrong').innerHTML = maxWrong;
-
+document.getElementById("maxWrong").innerHTML = maxWrong;
 
 
 randomWord();
