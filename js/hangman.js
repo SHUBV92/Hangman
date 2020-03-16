fa95565dff6459843f5aca327b3b2301fc8e4ba3
your Shubinder Virk <shubinder92@gmail.co.uk>
2020-03-16 15:09:02 +0000

    Added the Hangman imagaes

--- js/hangman.js
+++ js/hangman.js
@@ -18,6 +18,7 @@ let answer = "";
 let maxWrong = 6;
 let mistakes = 0;
 let guessed = [];
+let wordStatus = null;
 
 function randomWord() {
   answer =
@@ -31,15 +32,28 @@ function generateButtons() {
         `
         <button 
             class="btn btn-lg btn-primary m-2"
-            id='` + letter + `
-            onClick-"handleGuess('` + letter + `')"
+            id='` + letter + `'
+            onClick="handleGuess('` + letter + `')"
             >
                 ` + letter + `
                 </button>
-        `);
+        `).join(``);
 
         document.getElementById('keyboard').innerHTML = buttonsHTML;
-}
+
+    }
+
+
+    function guessedWord(){
+        wordStatus = answer.split('')
+        .map(letter => (guessed.indexOf(letter) >= 0 ? letter : "_")).join("");
+
+        document.getElementById('wordSpotLight').innerHTML = wordStatus;
+    }
+
+
+
+    document.getElementById('maxWrong').innerHTML = maxWrong;
 
 
 
